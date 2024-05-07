42a84f78d26be131356691c96ddd36f805801d08
djcode827 <dellojames827@gmail.com>
2024-05-06 20:46:50 -0700

    c1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
-
+import {BrowserRouter as Router, Routes, Link, Route} from "react-router-dom";
 import './App.css'
+import Home from "./components/Home";
+import Product from "./components/Product";
+import Customer from "./components/Customer";
 
 function App() {
 
 
   return (
-    <h1>Hello Team!</h1>
+    <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to={'/'}>Home</Link>
+              </li>
+              <li>
+                <Link to={'/customer'}>Customer</Link>
+              </li>
+              <li>
+                <Link to={'/product'}>Product</Link>
+              </li>
+              {/* <li></li>
+              <li></li> */}
+            </ul>
+          </nav>
+          <hr />
+
+
+              <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/customer" element={<Customer/>} />
+                <Route path="/product" element={<Product/>} />
+              </Routes>
+
+
+        </div>
+    </Router>
   )
 
 }
